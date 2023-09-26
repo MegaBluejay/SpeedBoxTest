3d9fb79a93af31c0babde0d009506f6b83230c71
MegaBluejay <dmoiseev2011@gmail.com>
2023-09-26 10:46:59 +0300

    Fix location code dto

--- SpeedBoxTest/CdekApi/Dtos/LocationInfoOutDto.cs
+++ SpeedBoxTest/CdekApi/Dtos/LocationInfoOutDto.cs
@@ -1,6 +1,9 @@
+using System.Text.Json.Serialization;
+
 namespace SpeedBoxTest.CdekApi.Dtos;
 
 public class LocationInfoOutDto
 {
+    [JsonPropertyName("code")]
     public int Code { get; set; }
 }
